a020f7ff718eba2ddacf7a6061eab2a1c03c99c9
naji-alhusami <najihussami@gmail.com>
2023-12-08 16:54:32 +0300

    added location to catch lan and lon for the address

---
+++ util/location.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const HttpError = require("../models/http-error");
+const API_KEY = "pk.1eb48a6676a97c6bca051dec02f79f90";
+
+async function getCoordsForAddress(address) {
+  const response = await axios.get(
+    `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${encodeURIComponent(
+      address
+    )}&format=json`
+  );
+
+  const data = response.data[0];
+
+  console.log(data);
+
+  if (!data || data.status === "ZERO_RESULTS") {
+    const error = new HttpError(
+      "Could not find location for the specified address.",
+      422
+    );
+    throw error;
+  }
+
+  const coorLat = data.lat;
+  const coorLon = data.lon;
+  const coordinates = {
+    lat: coorLat,
+    lng: coorLon,
+  };
+
+  return coordinates;
+}
+
+module.exports = getCoordsForAddress;
